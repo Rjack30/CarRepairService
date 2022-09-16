48bbba64c7d38cf416c6b24fea2242c5ab8471d8
Admin <roderickjackson358@yahoo.com>
2022-09-15 23:33:38 -0700

    added ConnectionPool class and tried connecting database

---
+++ src/main/java/Services/ConnectionPool.java
@@ -0,0 +1,39 @@
+package Services;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectionPool {
+
+    private static ConnectionPool pool;
+
+    private static final List<Connection> connectionPool = new ArrayList<>(5);
+
+    private ConnectionPool(){
+
+    }
+public static ConnectionPool getInstance() {
+    if (pool == null) {
+        pool = new ConnectionPool();
+        for (int i = 0; i < 5; i++) {
+            try {
+                connectionPool.add(DriverManager.getConnection("jdbc:mysql://localhost:3306/CarRepairService", "root", "Mybday0124"));
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    return pool;
+}
+
+public synchronized Connection getConnection(){
+        return connectionPool.remove(connectionPool.size()-1);
+}
+
+public synchronized void returnConnection(Connection connection){
+
+    }
+}
