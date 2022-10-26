7da1c99c340903827cb5839e6f045c6bf7f11164
Admin <roderickjackson358@yahoo.com>
2022-10-25 19:32:21 -0700

    Fix and updated Main class Fix updated Mechanic class

--- src/main/java/dao/Mechanic.java
+++ src/main/java/dao/Mechanic.java
@@ -1,18 +1,90 @@
 package dao;
-
+import java.sql.*;
 public class Mechanic {
 
-    public String name;
-    public String lastName;
-
-    public Mechanic(String name, String lastName) {
+    private String name;
+    public Mechanic (String name) {
         this.name = name;
-        this.lastName = lastName;
     }
-    @Override
-    public String toString() {
-        return "[" + name + " " + lastName + "]";
+    public String getName() {
+        return name;
     }
 
+    private static Connection connection;
+    private static Statement statement;
+    private static ResultSet rs;
+
+    public static void main () {
+        try {
+            String url = "jdbc:mysql://localhost:3306/mydb";
+            Connection conn = DriverManager.getConnection(url,"root","JRack22!#");
+            Statement st = conn.createStatement();
+            st.executeUpdate("DROP TABLE IF EXISTS Mechanic");
+            st.executeUpdate("CREATE TABLE Attorney (\n" +
+                    "    id INT AUTO_INCREMENT,\n" +
+                    "    first_name VARCHAR(255) NOT NULL,\n" +
+                    "    last_name VARCHAR(45) NOT NULL,\n" +
+                    "    phone_number VARCHAR(45) NOT NULL,\n" +
+                    "    email VARCHAR(45) NOT NULL,\n" +
+                    "    Customers_id INT NOT NULL,\n" +
+                    "    Offices_idOffices INT NOT NULL,\n" +
+                    "    Reviews_idReviews INT NOT NULL, \n" +
+                    "    PRIMARY KEY (id)\n" +
+                    ");"
+
+            );
+            st.executeUpdate("INSERT INTO Mechanic " +
+                    "VALUES (1002, 'Will', 'Wright', 'Motor Doctor', 'Engine', 'Burbank', '300' )");
+            st.executeUpdate("INSERT INTO Mechanic " +
+                    "VALUES (1003, 'Robert', 'Earl', 'Brake Service', 'Brakes', 'Burbank', '500' )");
+            st.executeUpdate("INSERT INTO Mechanic " +
+                    "VALUES (1004, 'Tim', 'Bonner', 'Suspension Service', 'Suspension', 'Burbank', '100' )");
+
+            conn.close();
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+
+    }
+    public static void setUp() {
+        String databaseURL = "jdbc:mysql://localhost:3306/mydb";
+        String user = "root";
+        String password = "JRack22!#";
+        connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Starting Connection to MYSQL Database");
+            System.out.println("Connecting to Database...");
+            connection = DriverManager.getConnection(databaseURL, user, password);
+            if (connection != null) {
+                System.out.println("Connected to the Database...");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void getAttorneysFromDataBase() {
+        try {
+            String query = "select * from Mechanic";
+            statement = connection.createStatement();
+            rs = statement.executeQuery(query);
+
+            while(rs.next()){
+                int Id= rs.getInt("Id");
+                String first_name= rs.getString("first_Name");
+                String last_name= rs.getString("last_Name");
+                String service = rs.getString("service");
+                String Office_id = rs.getString("Office_id");
+                System.out.println(Id+ " , "+ first_name + " " + last_name + " , " + service + " , " + Office_id);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -1,87 +1,52 @@
 package model;
 
-
-import dao.Car;
 import dao.Mechanic;
 
-import java.util.logging.Logger;
+import java.util.Scanner;
 
 public class Main {
-
-    private static final String Car_Repair_DEBUG = "@@@@@@@@@@@";
-    private static final String Car_Repair_WARN = "^^^^^^^^^^^";
-    private static final String Car_Repair_INFO = "*************";
-    private static final String Car_Repair_ERROR = "############";
-    private static final String Car_Repair_FATAL = "$$$$$$$$$$";
-    
-    private static java.util.logging.LogManager LogManager;
-    private static final Logger logger = LogManager.getLogger(String.valueOf(City.class));
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Thank You for Contacting Rod Auto-care Shop");
+        System.out.println("Are You Looking To Get Your Car Service today?");
+        System.out.println("[1] Yes");
+        System.out.println("[2] No");
 
-        var person1 = new Mechanic("Jonh", "Gold");
-        var person2 = new Mechanic("James", "Miller");
-        // create 2 cars
-        var car1 = new Car(" BMW M3", 2013, person1);
-        var car2 = new Car("Mercedes-Benz E550", 2017, person2);
+        int value = scanner.nextInt();
 
-        public static void debug (Logger Object String;
-        logger, String message){
-            logger.debug(Car_Repair_DEBUG + message + Car_Repair_DEBUG);
+        switch (value) {
+
+            case 1:
+                Mechanic.main();
+                Offices.main();
+                Payments.main();
+                System.out.println("\n");
+                Mechanic.setUp();
+                System.out.println("\n");
+                System.out.println("Thank you for choosing us! Here is a list of our current Mechanics  ");
+                Mechanic.getMechanicsFromDataBase();
+                System.out.println("\n");
+                Offices.setUp();
+                System.out.println("\n");
+                System.out.println("Here are the available Offices that our mechanics can work at");
+                Offices.getCourthousesFromDataBase();
+                System.out.println("\n");
+                Payments.setUp();
+                System.out.println("\n");
+                System.out.println("These are the current Payment Methods");
+                Payments.getPaymentFromDataBase();
+                System.out.println("\n");
+                break;
+
+            case 2:
+                System.out.println("We will be here whenever your car needs some work done to it!");
+                break;
+
         }
-
-        // logging
-        logger.debug(" Car_Repair_Debug + message");
-        logger.info("Person1: " + person1);
-        logger.info("Car2: " + car2);
-        logger.warning("Warning accrued at " + LocalDateTime.now());
-        logger.error("Error accrued at " + LocalDateTime.now());
-        logger.fatal("Serious problem with car " + car1 + " accrued at " + LocalDateTime.now());
-        Scanner input = new Scanner(System.in);
-
-        String[] offers = new String[]{"OilChange", "SuspensionServices",
-                "TransmissionRepair", "BrakeServices"};
-
-        int[] prices = {120, 80, 200, 75};
-
-        System.out.println("Enter Selection:");
-        String choice = input.nextLine();
-
-
-        for (int x = 0; x < prices.length; ++x) {
-            if (offers[x].equals(choice)) {
-                System.out.println(offers[x] + " price is $" + prices[x]);
-                return;
-            }
-        }
-        System.out.println("Invalid Entry");
     }
-
-    public static void main(String[] args) {
-
-        int yearModel = 0;
-        String make = null;
-
-        Scanner keyboard = new Scanner(System.in);
-
-        Car car = new Car();
-
-        System.out.println("What is the year of the car?");
-        yearModel = keyboard.nextInt();
-
-        System.out.println("What is the make of the car?");
-        keyboard.nextLine(); // fixes bug - receives the Enter key without skipping make = keyboard.nextLine()
-        make = keyboard.nextLine();
-
-        System.out.println("Your " + yearModel + " " + make +
-                " is traveling at ");
-        System.out.println("What is the year of your car? ");
-        yearModel = keyboard.nextInt();
-
-        System.out.println("What is the make of your car? ");
-        make = keyboard.next();
-
-
-    }
-
 }
+
+
+
+
